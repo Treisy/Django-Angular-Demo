fd2439325533564822ecf05f587673c17b12cc03
josefmonje <josefmonje@gmail.com>
2014-09-11 19:30:22 +0800

    Removed excess comments, reorganized some code.

--- products/static/js/app/products.js
+++ products/static/js/app/products.js
@@ -1,42 +1,41 @@
 (function() {
 'use strict;'
 
-// Define the service function, dependencies are injected.
+// This service deals with communicating with the product API for CRUD.
 productSvc = function($http, djangoUrl){
-  // List of methods provided by service as variables.
+  // API we are using for this service.
+  var _api = 'product_api'
+
   var create, list, read, update, remove
-  // Storage for current object
   var _curObject
 
-  // GET list of objects from the server
   this.list = function(callback){
-    $http.get(djangoUrl.reverse('product_api'))
-      // Callback was provided upon calling the function
+    $http.get(djangoUrl.reverse(_api))
       .success(callback)
+      // Callbacks were provided upon calling the function
   }
-  // POST a new object to the server
-  this.create = function(data, callback){
-    $http.post(djangoUrl.reverse('product_api'), data)
-      .success(callback)
-  }
-  // Update one object by POSTing to its URL
-  this.update = function(pk, data, callback){
-    $http.post(djangoUrl.reverse('product_api') + "?pk=" + pk, data)
+  this.read = function(pk, callback){
+    $http.get(djangoUrl.reverse(_api) + "?pk=" + pk)
       .success(callback)
     }
-  // GET objects from the server
-  this.read = function(pk, callback){
-    // Get an object with pk from array
+/* This is another method of retrieving one object
     this.list(function(data) {
       var object = data.filter(function(item){
         return item.pk == pk;
       })[0]
       callback(object);
     })
+*/
+  this.create = function(data, callback){
+    $http.post(djangoUrl.reverse(_api), data)
+      .success(callback)
   }
-  // DELETE an object from server
+  this.update = function(pk, data, callback){
+    $http.post(djangoUrl.reverse(_api) + "?pk=" + pk, data)
+      .success(callback)
+    }
   this.remove = function(pk, callback){
-    $http.delete(djangoUrl.reverse('product_api') + "?pk=" + pk)
+    $http.delete(djangoUrl.reverse(_api) + "?pk=" + pk)
       .success(callback)
     }
 }
@@ -49,7 +48,6 @@ angular.module('products', [])
     '$http',
     // URL reverse lookups provided by Django-Angular.
     'djangoUrl',
-    // Store logic in variable. When it causes an error, we know it's from here.
     productSvc,
   ])
 })();
--- products/static/js/app/routes.js
+++ products/static/js/app/routes.js
@@ -1,42 +1,8 @@
 (function() {
 'use strict;'
 
-angular.module('routes', [   // module name
-  'ngRoute',                 // Angular.js routing module
-  'products'                 // Think of this like imports
-  ])
-  .config(['$routeProvider', // Think of this like Django URLS
-    function($routeProvider) { 
-      $routeProvider
-        .when('/', {
-          templateUrl: 'includes/hero.html',
-        })
-        .when('/products/', {
-          templateUrl: 'includes/list.html',
-          controllerAs: 'list',
-          controller: 'productListCtrl'
-        })
-        .when('/products/new/', {
-          templateUrl: 'includes/form.html',
-          controllerAs: 'product',
-          controller: 'productFormCtrl'
-        })
-        .when('/products/new/:productPk/', {
-          templateUrl: 'includes/form.html',
-          controllerAs: 'product',
-          controller: 'productFormCtrl',
-        })
-        .when('/products/:productPk/', {
-          templateUrl: 'includes/product.html',
-          controllerAs: 'detail',
-          controller: 'productDetailCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
-        })
-    }])
-
 productListCtrl = function($scope, productSvc) {
+  productSvc._curObject = undefined
   productSvc.list(function(data){
     $scope.products = data;
   })
@@ -44,40 +10,35 @@ productListCtrl = function($scope, productSvc) {
 
 productFormCtrl = function($location, $routeParams, $scope, productSvc) {
   var save
-  var intTypes = [ 'size', ]
+  // List of keys in model which have numerical values.
+  var numTypes = [
+    'size',
+  ]
 
-  // Check if coming from product list as edit
   if ($routeParams.productPk != undefined) {
-    // Redirect if object editing was accessed directly
     if (productSvc._curObject == undefined) {
       $location.path("/products/")
     }
+
     object = productSvc._curObject
-    // Bind object keys and values to scope
     for (key in object) {
       if (object.hasOwnProperty(key)) {
-        // Based on keys in array of intTypes, bind as number or string.
-        intTypes.indexOf(key) >= 0 ? this[key] = parseInt(object[key]) : this[key] = object[key]
+        // Bind to form as number or string as needed.
+        numTypes.indexOf(key) >= 0 ? this[key] = parseInt(object[key]) : this[key] = object[key]
       }
     }
-    // Scope has save functionality
-    // Save means update
+
     this.save = function(){
-    // Access Product Service for update functionality
       productSvc.update($routeParams.productPk, $scope.product, function(data) {
         $scope.response = data;
       })
-      // Redirect after action
       $location.path("/products/")
     }
   } else {
-    // Save means new object
     this.save = function(){
-    // Access Product Service for create functionality
       productSvc.create($scope.product, function(data){
         $scope.response = data;
       })
-      // Redirect after action
       $location.path("/products/")
     }
   }
@@ -86,22 +47,61 @@ productFormCtrl = function($location, $routeParams, $scope, productSvc) {
 productDetailCtrl = function($location, $routeParams, $scope, productSvc) {
   var remove
 
-  // Access Product Service for read functionality
   productSvc.read($routeParams.productPk, function(data) {
-    // Bind object to scope
+    if (data.message == "Product matching query does not exist.") {
+      $scope.response = data.message
+      $location.path("/products/")
+    }
     $scope.product = data
-    // Store object for use in editing, no need to get from server
     productSvc._curObject = data
   })
-  // Scope has remove functionality
+
   this.remove = function(pk){
     this.remove = productSvc.remove(pk, function(data) {
-      // Server response
       $scope.response = data
     })
-    // Redirect after action
     $location.path("/products/")
   }
 }
+
+// Think of this like Django URLS.
+URLS = function($routeProvider) { 
+  $routeProvider
+    .when('/', {
+      templateUrl: 'includes/hero.html',
+    })
+    .when('/products/', {
+      templateUrl: 'includes/list.html',
+      controllerAs: 'list',
+      controller: 'productListCtrl'
+    })
+    .when('/products/new/', {
+      templateUrl: 'includes/form.html',
+      controllerAs: 'product',
+      controller: 'productFormCtrl'
+    })
+    .when('/products/new/:productPk/', {
+      templateUrl: 'includes/form.html',
+      controllerAs: 'product',
+      controller: 'productFormCtrl',
+    })
+    .when('/products/:productPk/', {
+      templateUrl: 'includes/product.html',
+      controllerAs: 'detail',
+      controller: 'productDetailCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+}
+
+// This is the module name.
+angular.module('routes', [
+  // Angular.js routing module.
+  'ngRoute',
+  // Think of this like imports.
+  'products'
+  ])
+  .config(URLS)
 
 })();
